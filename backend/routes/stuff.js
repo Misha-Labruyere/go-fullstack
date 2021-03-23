7c1b59416433873f61f4d736e726cbcf690978d6
Misha-Labruyere <mishalabruyere@gmail.com>
2021-03-23 13:19:48 +0100

    backend

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post("/", stuffCtrl.createThing);
-router.put("/:id", stuffCtrl.modifyThing);
-router.delete("/:id", stuffCtrl.deleteThing);
-router.get("/:id", stuffCtrl.getOneThing);
-router.use("/", stuffCtrl.getAllThings);
+router.post("/", auth, multer, stuffCtrl.createThing);
+router.put("/:id", auth, multer, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+router.get("/:id", auth, stuffCtrl.getOneThing);
+router.use("/", auth, stuffCtrl.getAllThings);
 
 module.exports = router;
